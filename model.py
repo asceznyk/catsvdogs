18a2efc9d0a9ca44a8806d7ad6743dae37f6054a
asceznyk <asceznyk@gmail.com>
2021-08-15 16:59:16 +0530

    some model

--- model.py
+++ model.py
@@ -9,7 +9,5 @@ def init_model():
     model = EfficientNet.from_pretrained('efficientnet-b0')
     model._fc = nn.Linear(model._fc.in_features, 1)
 
-    model.to(device)
-
-    return model, train_loader, test_loader
+    return model.to(config.device)
 
