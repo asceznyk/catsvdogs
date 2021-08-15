43fcbfaf5104e55257c92d3f43fbb16736425bae
asceznyk <asceznyk@gmail.com>
2021-08-15 12:40:17 +0530

    logisitc regrssor added!

---
+++ regression.py
@@ -0,0 +1,38 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+
+import sklearn
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss, accuracy_score
+
+x = np.load(f'/content/trainfeatures.npy')
+y = np.load(f'/content/trainlabels.npy')
+
+print(x.shape, y.shape)
+
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.001, random_state=1337)
+clf = LogisticRegression(max_iter=2000)
+clf.fit(x_train, y_train)
+
+p_train = clf.predict_proba(x_train)[:, 1]
+loss = log_loss(y_train, p_train)
+acc = clf.score(x_val, y_val)
+
+print(f'log loss: {loss}')
+print(f'accuaracy score: {acc}')
+
+x_test = np.load('/content/testfeatures.npy')
+
+print(x_test.shape)
+
+p_test = clf.predict_proba(x_test)[:, 1]
+df = pd.DataFrame({'id': np.arange(1, 12501), 'label': np.clip(p_test, 0.005, 0.995)})
+df.to_csv('yosubmission.csv', index=0)
+
+filename = 'clf.log.regressor'
+pickle.dump(clf, open(filename, 'wb'))
+print(f'saving classifier to file {filename}')
